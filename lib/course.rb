0aa0232b5c2e54f0e143ebc968f095d49223f5eb
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-10-30 18:02:39 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -3,5 +3,15 @@ class Course
 
   @all = []
 
-  def initialize(title, schedule, description)
+  def initialize
+    @@all << self
+  end
+
+  def self.all 
+    @@all
+  end
+
+  def self.reset.all 
+    @@all.clear
+  end
 end
